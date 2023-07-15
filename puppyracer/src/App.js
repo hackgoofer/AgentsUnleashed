ff48a4d1576f99f58624b658d384e4b8e0e4349a
reibs <areibman@gmail.com>
2023-07-15 14:00:59 -0700

    timeline

--- puppyracer/src/App.js
+++ puppyracer/src/App.js
@@ -19,6 +19,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="Timeline" /> {/* Add this line */}
         <img
           src={corgiGif}
           className="Corgi"
