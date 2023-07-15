ec58c966c7ff8f78ac33013d2b2ed080d3593b84
reibs <areibman@gmail.com>
2023-07-15 13:43:38 -0700

    corgi sit

--- puppyracer/src/App.js
+++ puppyracer/src/App.js
@@ -1,19 +1,27 @@
 import React, { useState } from "react";
 import "./App.css";
-import corgi from "./corgi.gif"; // Make sure to import your corgi gif
+import corgiRun from "./corgi_run.gif";
+import corgiSit from "./corgi_sit.gif";
 
 function App() {
-  const [position, setPosition] = useState(0); // Initialize position state
+  const [position, setPosition] = useState(0);
+  const [isMoving, setIsMoving] = useState(false);
 
   const handleButtonClick = () => {
+    setIsMoving(true);
     setPosition((oldPosition) => oldPosition + 1); // Update position
+
+    // After a delay, stop moving
+    setTimeout(() => {
+      setIsMoving(false);
+    }, 500); // Adjust this delay as necessary
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <img
-          src={corgi}
+          src={isMoving ? corgiRun : corgiSit}
           className="Corgi"
           style={{ left: `${position * 50}px` }} // Adjust as necessary
           alt="corgi"
