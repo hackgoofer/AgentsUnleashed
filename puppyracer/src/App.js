a2cafb21c425f6a0e7362be3680635d50c25aa64
reibs <areibman@gmail.com>
2023-07-15 14:54:28 -0700

    structured tasks

--- puppyracer/src/App.js
+++ puppyracer/src/App.js
@@ -1,35 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import corgiRun from "./corgi_run.gif";
 import corgiSit from "./corgi_sit.gif";
 
 function App() {
-  const [position, setPosition] = useState(0);
-  const [corgiGif, setCorgiGif] = useState(corgiSit);
+  const tasks = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"];
+  const [currentTask, setCurrentTask] = useState(0);
+  const [isMoving, setIsMoving] = useState(false);
+
+  useEffect(() => {
+    if (isMoving) {
+      const timer = setTimeout(() => {
+        setIsMoving(false);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [isMoving]);
 
   const handleButtonClick = () => {
-    setCorgiGif(corgiRun);
-    setPosition((oldPosition) => oldPosition + 100); // Update position
-  };
-
-  const handleTransitionEnd = () => {
-    setCorgiGif(corgiSit);
+    setIsMoving(true);
+    setCurrentTask((oldTask) =>
+      oldTask + 1 < tasks.length ? oldTask + 1 : oldTask
+    );
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        {corgiGif === corgiSit && (
-          <div className="ChatBubble" style={{ left: `${position}px` }}>
+        <div className="Timeline">
+          {tasks.map((task, index) => (
+            <div
+              key={task}
+              className="TimelineNode"
+              style={{ left: `${(index / (tasks.length - 1)) * 100}%` }}
+            />
+          ))}
+        </div>
+        {!isMoving && currentTask !== 0 && (
+          <div
+            className="ChatBubble"
+            style={{ left: `${(currentTask / (tasks.length - 1)) * 100}%` }}
+          >
             Hello!
           </div>
         )}
-        <div className="Timeline" />
         <img
-          src={corgiGif}
+          src={isMoving ? corgiRun : corgiSit}
           className="Corgi"
-          style={{ transition: "left 2s", left: `${position}px` }}
-          onTransitionEnd={handleTransitionEnd}
+          style={{
+            transition: "left 2s",
+            left: `${(currentTask / (tasks.length - 1)) * 100}%`,
+          }}
           alt="corgi"
         />
         <button onClick={handleButtonClick}>Move Corgi</button>
